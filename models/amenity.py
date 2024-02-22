9dcbd56fee6dfbb57ffa69299e2a0be0ab03dfb1
chaimaeboultam <chaimaeboultam2002@gmail.com>
2024-02-22 08:12:40 -0800

    .

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Table
+from sqlalchemy.orm import relationship
+import os
 
 
 class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
-    __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    """Class Amenity"""
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship(
+            'Place', secondary='place_amenity',
+            back_populates="amenities",
+            viewonly=False)
